d9ed51bc0ebb56738208b6348e9cebfd1511d2bc
Will Purvis <williampurvis00@gmail.com>
2024-03-13 16:00:34 +0000

    custom DICOM dataclass

---
+++ src/data.py
@@ -0,0 +1,75 @@
+"""
+Defines custom PyTorch datasets for the DICOM data
+"""
+
+import torch
+from torch.utils.data import Dataset
+
+
+# TODO: check if data is normalised
+
+
+class DICOMSliceDataset(Dataset):
+    def __init__(self, data_dict, segmentation_dict, transform=None):
+        """
+        Parameters
+        ----------
+        data_dict : dict
+            Dictionary of patient IDs to their 3D DICOM data numpy arrays
+        segmentation_dict : dict
+            Dictionary of patient IDs to their 3D segmentation mask numpy arrays
+        transform : callable, optional
+            Optional transform to be applied on a sample.
+        """
+        self.slices = []
+        self.masks = []
+        self.transform = transform
+
+        # Flatten 3D data into 2D slices
+        for patient_id, data in data_dict.items():
+            num_slices = data.shape[0]
+            for slice_idx in range(num_slices):
+                self.slices.append(data[slice_idx, :, :])
+                self.masks.append(segmentation_dict[patient_id][slice_idx, :, :])
+
+    def __len__(self):
+        """
+        Returns the length of the dataset.
+
+        Returns
+        -------
+        int
+            Length of the dataset.
+        """
+        return len(self.slices)
+
+    def __getitem__(self, idx):
+        """
+        Retrieves the slice and mask at the given index.
+
+        Parameters
+        ----------
+        idx : int
+            Index of the slice and mask to retrieve.
+
+        Returns
+        -------
+        torch.Tensor
+            A tensor representing the slice.
+        """
+        slice = self.slices[idx]
+        mask = self.masks[idx]
+
+        if self.transform:
+            slice = self.transform(slice)
+            mask = self.transform(mask)
+
+        # Convert numpy arrays to torch tensors
+        slice = torch.from_numpy(slice).float()
+        mask = torch.from_numpy(mask).float()
+
+        # Add a channel dimension to the slice [1, y, z]
+        slice = slice.unsqueeze(0)
+        mask = mask.unsqueeze(0)
+
+        return slice, mask
